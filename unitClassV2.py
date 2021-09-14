e2e61f2fa9095981f073ede3270b220155453bfd
Erle Guillemot <gwerle@bleiz.home>
2021-09-14 16:59:23 +0200

    premier jet pour la classe unité

---
+++ unitClassV2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from element import *
+
+#############################
+#####     Classes       #####
+#############################
+
+class Team:
+
+    def __init__(self, position, board, team):
+        
