6f821b86c0a7ab1a7f749328bfdbb041352786b4
wildtchoupi <mel.goy42@outlook.fr>
2021-11-18 21:47:39 +0100

    nouveaux batiments

---
+++ AoE-pygame/Building/Ferme.py
@@ -0,0 +1,22 @@
+from Batiment import *
+
+class Ferme(Batiment):
+
+    def __init__(self, pos, team):
+        ### Tout ce que fait une Ferme ###
+        self.pv = 480
+        self.job = "ferme"
+        self.action = "Neant"
+        self.sight = 1
+        self.needWood = 75
+
+        pygame.sprite.Sprite.__init__(self)
+        self.frame = 0
+        self.images = []
+        img = pygame.image.load(os.path.join("Building/Farm.png")).convert()
+        img.convert_alpha()  # optimise alpha
+        img.set_colorkey(ALPHA)  # set alpha
+        self.images.append(img)
+        self.image = self.images[0]
+        self.rect = self.image.get_rect()
+        super().__init__(pos, team);
---
+++ AoE-pygame/Building/TourArcher.py
@@ -0,0 +1,23 @@
+from Batiment import *
+
+class TourArcher(Batiment):
+
+    def __init__(self, pos, team):
+        ### Tout ce que fait une Tour d'archer ###
+        self.pv = 700
+        self.job = "tourarcher"
+        self.action = "Neant"
+        self.sight = 10
+        self.needGold = 50
+        self.needStone = 120
+
+        pygame.sprite.Sprite.__init__(self)
+        self.frame = 0
+        self.images = []
+        img = pygame.image.load(os.path.join("Building/ArcherTower.png")).convert()
+        img.convert_alpha()  # optimise alpha
+        img.set_colorkey(ALPHA)  # set alpha
+        self.images.append(img)
+        self.image = self.images[0]
+        self.rect = self.image.get_rect()
+        super().__init__(pos, team);
