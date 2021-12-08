46082ed3dfa6affae4694ec0ab65e2eccd057e7f
Camille <herrero.camille82@gmail.com>
2021-12-08 21:04:19 +0100

    debut merge quentin

--- view/Map_erle.py
+++ view/Map_erle.py
@@ -1,4 +1,19 @@
+import os
+
+import jsonpickle
 import pygame
+from matplotlib import image as mpimg
+
+from model.Unit.Champion import Champion
+from model.Unit.King import King
+from model.Unit.Villager import Villager
+from model.building import TourArcher, Barracks, House
+from model.building.Forum import Forum
+from model.building.GoldMine import GoldMine
+from model.building.Sheep import Sheep
+from model.building.StoneMine import StoneMine
+from model.building.Tree import Tree
+from resources import ElementsColor
 from resources.game_constants import *
 
 class MapE():
@@ -39,3 +54,80 @@ class MapE():
         self.update_afg()
 
         return True
+
+    @staticmethod
+    def get_board_from_json(json_file_path):
+        json_file = open(json_file_path, 'r')
+        json_content = json_file.read()
+        json_file.close()
+        return jsonpickle.decode(json_content)
+
+    def create_map_from_file(self, file_path, joueur):
+        print(file_path[file_path.rfind('.'):])
+        # if file_path[file_path.rfind('.'):] == "json":
+        #     map = MapE()
+        #     map.board = MapE.get_board_from_json(file_path)
+        #     self.update_images(map.board)
+        #     return map
+
+        img_script = mpimg.imread('resources/map/png/' + file_path).tolist()
+
+        board = MapE()
+
+        for y, line in enumerate(img_script):
+            for x, column in enumerate(line):
+                if column == ElementsColor.Color.TREE.value:
+                    board.board.append(Tree((x * BASE, y * BASE), None, board))
+                elif column == ElementsColor.Color.GOLD_MINE.value:
+                    board.board.append(GoldMine((x * BASE, y * BASE), None, board))
+                elif column == ElementsColor.Color.STONE_MINE.value:
+                    board.board.append(StoneMine((x * BASE, y * BASE), None, board))
+                elif column == ElementsColor.Color.TOWN_CENTER.value:
+                    board.board.append(Forum((x * BASE, y * BASE), None, joueur, board))
+                elif column == ElementsColor.Color.KING.value:
+                    board.board.append(King((x * BASE, y * BASE), "R", board))
+                elif column == ElementsColor.Color.CHAMPION.value:
+                    board.board.append(Champion((x * BASE, y * BASE), 'R', board))
+                elif column == ElementsColor.Color.VILLAGEOIS.value:
+                    board.board.append(Villager((x * BASE, y * BASE), 'B', board))
+                elif column == ElementsColor.Color.SHEEP.value:
+                    board.board.append(Sheep((x * BASE, y * BASE), None, board))
+
+        # board.create_json_file(file_path[0:file_path.rfind('.')])
+
+        return board
+
+    def create_json_file(self, file_name):
+        file = open('resources/map/json/' + file_name + '.json', 'w')
+        file.write(jsonpickle.encode(self.board))
+        file.close()
+
+    def update_images(self, board):
+        for el in board:
+            image = None
+            if type(el) is Tree:
+                image = pygame.image.load(os.path.join("model/building/images/Tree.png")).convert()
+            elif type(el) is GoldMine:
+                image = pygame.image.load(os.path.join("model/building/images/GoldMine.png")).convert()
+            elif type(el) is Forum:
+                image = pygame.image.load(os.path.join("model/building/images/Towncenter.png")).convert()
+            elif type(el) is StoneMine:
+                image = pygame.image.load(os.path.join("model/building/images/StoneMine.png")).convert()
+            elif type(el) is Villager:
+                image = pygame.image.load(os.path.join("model/Unit/images/villager.png")).convert()
+            elif type(el) is TourArcher:
+                image = pygame.image.load(os.path.join("model/building/images/TourArcher.png")).convert()
+            elif type(el) is Barracks:
+                image = pygame.image.load(os.path.join("model/building/images/Barracks.png")).convert()
+            elif type(el) is House:
+                image = pygame.image.load(os.path.join("model/building/images/House.png")).convert()
+            elif type(el) is King:
+                image = pygame.image.load(os.path.join("model/Unit/images/king.png")).convert()
+            elif type(el) is Champion:
+                image = pygame.image.load(os.path.join("model/Unit/images/champion.png")).convert()
+            if type(el) is Forum:
+                image = pygame.transform.scale(image, (BASE * el.size, BASE * el.size))
+            else:
+                print(type(el))
+
+            el.image = image
